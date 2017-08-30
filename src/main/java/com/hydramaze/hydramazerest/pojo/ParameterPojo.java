1fd0b5407f67eda5f5be6759dd1bd19b0ae651cb
mvgnunes <marcustrs@gmail.com>
2017-08-30 01:02:51 -0300

    criado a API para executar o script

---
+++ src/main/java/com/hydramaze/hydramazerest/pojo/ParameterPojo.java
@@ -0,0 +1,22 @@
+package com.hydramaze.hydramazerest.pojo;
+
+public class ParameterPojo {
+    private Integer parameterId;
+    private String value;
+
+    public Integer getParameterId() {
+        return parameterId;
+    }
+
+    public void setParameterId(Integer parameterId) {
+        this.parameterId = parameterId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/hydramaze/hydramazerest/service/impl/AlgorithmExecuterService.java
@@ -0,0 +1,16 @@
+package com.hydramaze.hydramazerest.service.impl;
+
+import com.hydramaze.hydramazerest.pojo.ParameterPojo;
+import com.hydramaze.hydramazerest.service.IAlgorithmExecuterService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlgorithmExecuterService implements IAlgorithmExecuterService {
+
+    @Override
+    public void executeScript(Integer algorithmId, List<ParameterPojo> pojo) {
+
+    }
+}
