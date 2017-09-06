c518b17663d853e17c8f9842369dfc4bb0835c48
marcospf-eng <marcospf.eng@gmail.com>
2017-09-05 22:24:36 -0300

    Added new control to timing execution of process builder

--- src/main/java/com/hydramaze/hydramazerest/business/PythonBusiness.java
+++ src/main/java/com/hydramaze/hydramazerest/business/PythonBusiness.java
@@ -7,15 +7,17 @@ import org.json.JSONObject;
 import java.io.*;
 import java.net.URLDecoder;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class PythonBusiness {
 
     private static final String SCRIPTS_FOLDER_NAME = "pyscripts";
+    private static final int PROCESS_TIMEOUT = 60;
 
     private Process process;
     private ProcessBuilder processBuilder;
     private JSONObject jsonObjectResult = null;
-    private double estimatedTime;
+    private double elapsedTime;
 
     // Public Methods
     public PythonBusiness() { /* Do nothing */ }
@@ -25,11 +27,15 @@ public class PythonBusiness {
             processBuilder(pythonRequest.getCommand());
             long startTime = System.nanoTime();
             process = processBuilder.start();
-            process.waitFor();
-            estimatedTime = (System.nanoTime() - startTime) / 1000000000.0;
-            printProcessOutput();
-            //getOutputAsJSONObject();
-            System.out.println("Time elapsed: " + estimatedTime);
+
+            if(!process.waitFor(PROCESS_TIMEOUT, TimeUnit.SECONDS)) {
+                process.destroy();
+            } else {
+                getOutputAsJSONObject();
+            }
+
+            elapsedTime = TimeUnit.MILLISECONDS.convert((System.nanoTime() - startTime), TimeUnit.NANOSECONDS) / 1000.0;
+            System.out.println("Time elapsed: " + elapsedTime);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -43,8 +49,8 @@ public class PythonBusiness {
         }
     }
 
-    public double getEstimatedTime() {
-        return estimatedTime;
+    public double getElapsedTime() {
+        return elapsedTime;
     }
 
     // Private Methods
@@ -67,14 +73,15 @@ public class PythonBusiness {
                 System.out.println(line);
             }
             in.close();
-        }catch (Exception e){
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    private JSONObject getOutputAsJSONObject() {
+    private void getOutputAsJSONObject() {
         try {
-            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(process.getInputStream(), "UTF-8"));
 
             String last = null;
             String line;
@@ -95,8 +102,9 @@ public class PythonBusiness {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return jsonObjectResult;
     }
 
+    public JSONObject getJsonObjectResult() {
+        return jsonObjectResult;
+    }
 }
