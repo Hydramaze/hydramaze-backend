4c1e29b853a6f334b4368fc6e4ca2cf9b578503d
mvgnunes <marcustrs@gmail.com>
2017-09-07 01:22:57 -0300

    criado API de dataSet

--- src/main/java/com/hydramaze/hydramazerest/controller/DataSetController.java
+++ src/main/java/com/hydramaze/hydramazerest/controller/DataSetController.java
@@ -1,13 +1,15 @@
 package com.hydramaze.hydramazerest.controller;
 
+import com.hydramaze.hydramazerest.model.DataSet;
+import com.hydramaze.hydramazerest.service.IDataSetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/data-set")
@@ -15,14 +17,18 @@ public class DataSetController {
 
     private static final Logger LOG = LoggerFactory.getLogger(DataSetController.class);
 
+    @Autowired
+    private IDataSetService dataSetService;
+
     private String getApiName() {
         return "data-set";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody ResponseEntity getDataSet() {
+    @RequestMapping(value = "getByAlgorithmId", method = RequestMethod.GET)
+    public @ResponseBody ResponseEntity getDataSetListByAlgorithmId(@RequestParam(required = true) Integer id) {
         try{
-            return new ResponseEntity<>("Success", HttpStatus.OK);
+            List<DataSet> response = dataSetService.getDataSetsByAlgorithmId(id);
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } catch (final Exception exception){
             LOG.error("[GET] /api/{} - {}", getApiName(), exception.getMessage());
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
